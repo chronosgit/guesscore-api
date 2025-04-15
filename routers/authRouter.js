3b559a7ecfc098f3b31bc83b1447b7be718f9fce
chronosgit <warfacemeister@mail.ru>
2025-04-15 20:47:44 +0500

    ✨ feat: added logout functionality

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const LoginHandler = require('@/handlers/auth/LoginHandler');
 const RegistrationHandler = require('@/handlers/auth/RegistrationHandler');
 const RefreshTokensHandler = require('@/handlers/auth/RefreshTokensHandler');
+const LogoutHandler = require('@/handlers/auth/LogoutHandler');
 
 const authRouter = express.Router();
 
@@ -10,5 +11,7 @@ authRouter.post('/login', LoginHandler);
 authRouter.post('/register', RegistrationHandler);
 
 authRouter.get('/refresh', RefreshTokensHandler);
+
+authRouter.get('/logout', LogoutHandler);
 
 module.exports = authRouter;
