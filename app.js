1ee950d34cee704e3259300ad77e6e5f0d0dad2c
chronosgit <warfacemeister@mail.ru>
2025-04-17 20:23:12 +0500

    🔧 config: added cors config in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 require('module-alias/register');
@@ -18,8 +19,22 @@ const startServer = async () => {
 		process.exit(-1);
 	}
 
+	const allowedOrigins = ['http://localhost:5173'];
+
 	const app = express();
 
+	app.use(
+		cors({
+			origin: function (origin, callback) {
+				if (!origin || allowedOrigins.includes(origin)) {
+					callback(null, true);
+				} else {
+					callback(new Error('Not allowed by CORS'));
+				}
+			},
+			credentials: true,
+		}),
+	);
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(cookieParser());
